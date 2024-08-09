a3ea2cdfd82de5edd835f9ba2ea375e7b96072f8
naman mishra <namanmish1234@gmail.com>
2024-08-09 16:07:03 +0530

    Initial commit with React project

--- src/components/Experience/exp.js
+++ src/components/Experience/exp.js
@@ -2,7 +2,6 @@ import React from 'react'
 import './exp.css';
 import UIdesign from '../../assets/cn.png';
 import Webdev from '../../assets/is.png'
-// import Appd from '../../assets/app-design.png';
 const Exp = () => {
   return (
     <section  id='exp' >
@@ -14,23 +13,17 @@ const Exp = () => {
             <div className='skillbartxt'>
               <h2>Teaching Assistant(Coding Ninjas)</h2>
               <p>As a Teaching Assistant at Coding Ninjas, I guided students, particularly in DSA and competitive coding courses. I addressed their queries, offering detailed explanations, and successfully completed my internship, earning a recognition
-<a id="chromeLink" href="https://drive.google.com/file/d/1ZgIrm3qFwclaQ_F5HuBMoiqZd2Nom-UM/view?usp=drive_link" target="_blank">certificate</a></p>
+<a id="chromeLink" href="https://drive.google.com/file/d/1VCtx1wRZFkYDGF0DJdTP3XRFK3GFYaj4/view?usp=drive_link" target="_blank"> certificate</a></p>
             </div>
         </div>
         <div className='skillbar'>
             <img src={Webdev} alt='Webdev' className='skillbarimage' />
             <div className='skillbartxt'>
               <h2>Student Ambassedor(Internshala)</h2>
-              <p>As a college brand ambassador for<a href="https://drive.google.com/file/d/1HlulWJ7JpXGSsG7J37tUFqtU8Ja3bzVf/view?usp=drive_link" className='internshala'> Internshala .</a>, I connected students to valuable internships through workshops and events, fostering professional growth and expanding my network.</p>  
+              <p>As a college brand ambassador for<a href="https://drive.google.com/file/d/1HlulWJ7JpXGSsG7J37tUFqtU8Ja3bzVf/view?usp=drive_link" className='internshala' target='_blank'> Internshala .</a>, I connected students to valuable internships through workshops and events, fostering professional growth and expanding my network.</p>  
             </div>
         </div>
-        {/* <div className='skillbar'>
-            <img src={Appd} alt='Appd' className='skillbarimage' />
-            <div className='skillbartxt'>
-              <h2>Python developer</h2>
-              <p> dummy textdummy textdummy textdummy textdummy text</p>  
-            </div>
-        </div> */}
+       
       </div>
     </section>
     )
--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './contact.css'
+import './contact.css';
 
 class ContactForm extends Component {
   constructor(props) {
@@ -19,11 +19,10 @@ class ContactForm extends Component {
     this.setState({ [name]: value });
   };
 
-  handleSubmit = (event) => {
+  handleSubmit = async (event) => {
     event.preventDefault();
     const { firstName, lastName, phoneNumber, email, message } = this.state;
-
-    // Check if all mandatory fields are filled
+  
     if (!firstName || !lastName || !phoneNumber || !email || !message) {
       const errors = {
         firstName: !firstName,
@@ -34,85 +33,82 @@ class ContactForm extends Component {
       };
       this.setState({ errors });
     } else {
-      // All fields are filled, you can submit the form here
-      console.log('Form submitted:', this.state);
-      // Add your submission logic here
+      await new Promise(resolve => setTimeout(resolve, 100));
+      window.location.reload(true);
     }
   };
+  
 
   render() {
     const { firstName, lastName, phoneNumber, email, message, errors } = this.state;
 
     return (
-        <section className='contactme'>
-             <h2>Contact Me</h2>
-      <div className='contact'>
-       
-        <form onSubmit={this.handleSubmit}>
+      <section className='contactme'>
+        <h2>Contact Me</h2>
+        <div className='contact'>
+          <form onSubmit={this.handleSubmit}>
             <div className='name'>
-          <div className='bothname'>
-            <label htmlFor="firstName" className='cname' >First Name</label>
-            <input
-              type="text"
-              id="firstName"
-              name="firstName"
-              required
-              value={firstName}
-              onChange={this.handleInputChange}
-              
-            />
-            {errors.firstName && <span className="error">First name is required</span>}
-          </div>
-          <div className='bothname'>
-            <label htmlFor="lastName" className='cname'>Last Name</label>
-            <input
-              type="text"
-              id="lastName"
-              name="lastName"
-              value={lastName}
-              onChange={this.handleInputChange}
-            
-            />
-            {errors.lastName && <span className="error">Last name is required</span>}
-          </div>
-          </div>
-          <div className='phone' >
-            <label htmlFor="phoneNumber" className='cname'>Phone</label>
-            <input
-              type="text"
-              id="phoneNumber"
-              name="phoneNumber"
-              value={phoneNumber}
-              onChange={this.handleInputChange}
-            />
-            {errors.phoneNumber && <span className="error">Phone number is required</span>}
-          </div>
-          <div className='emailmsg'>
-            <label htmlFor="email" className='cname'>Email</label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              value={email}
-              onChange={this.handleInputChange}
-            />
-            {errors.email && <span className="error">Email is required</span>}
-          </div>
-          <div className='msg'>
-            <label htmlFor="message" className='cname'>Message</label>
-            <textarea
-              id="message"
-              name="message"
-              value={message}
-              onChange={this.handleInputChange}
-            />
-            {errors.message && <span className="error">Message is required</span>}
-          </div>
-          <div>
-            <button type="submit" className='button'>Submit</button>
-          </div>
-        </form>
-      </div>
+              <div className='bothname'>
+                <label htmlFor="firstName" className='cname'>First Name</label>
+                <input
+                  type="text"
+                  id="firstName"
+                  name="firstName"
+                  required
+                  value={firstName}
+                  onChange={this.handleInputChange}
+                />
+                {errors.firstName && <span className="error">First name is required</span>}
+              </div>
+              <div className='bothname'>
+                <label htmlFor="lastName" className='cname'>Last Name</label>
+                <input
+                  type="text"
+                  id="lastName"
+                  name="lastName"
+                  value={lastName}
+                  onChange={this.handleInputChange}
+                />
+                {errors.lastName && <span className="error">Last name is required</span>}
+              </div>
+            </div>
+            <div className='phone'>
+              <label htmlFor="phoneNumber" className='cname'>Phone</label>
+              <input
+                type="text"
+                id="phoneNumber"
+                name="phoneNumber"
+                value={phoneNumber}
+                onChange={this.handleInputChange}
+              />
+              {errors.phoneNumber && <span className="error">Phone number is required</span>}
+            </div>
+            <div className='emailmsg'>
+              <label htmlFor="email" className='cname'>Email</label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                value={email}
+                onChange={this.handleInputChange}
+              />
+              {errors.email && <span className="error">Email is required</span>}
+            </div>
+            <div className='msg'>
+              <label htmlFor="message" className='cname'>Message</label>
+              <textarea
+                id="message"
+                name="message"
+                value={message}
+                onChange={this.handleInputChange}
+              />
+              {errors.message && <span className="error">Message is required</span>}
+            </div>
+            <div>
+              <button type="submit" className='button'>Submit</button>
+            </div>
+          </form>
+        </div>
       </section>
     );
   }
